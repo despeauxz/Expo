3f18ef0e39231246953bda34c4d8d001b53e927c
Malik Godwin <despeauxz@gmail.com>
2019-08-28 05:08:37 +0100

    chore-[168132088]: continous integration
    - setup coveralls and travis
    - write test for the application
    
    [Finishes #168132088]

---
+++ server/tests/middlewares/errorHandler.spec.js
@@ -0,0 +1,45 @@
+import sinon from 'sinon';
+import { assert } from 'chai';
+import errorHandler from '@middlewares/errorHandler';
+
+const jsonFunc = sinon.spy();
+
+const res = {
+  headersSent: false,
+  status: (status) => ({
+    json: (message) => ({
+      status,
+      message,
+    }),
+  }),
+  json: jsonFunc,
+};
+
+const req = {
+  headers: 'header',
+};
+
+const err = {
+  status: 422,
+  message: 'error',
+};
+
+const next = sinon.spy();
+
+describe('Error Handler', () => {
+  afterEach(() => {
+    process.env.NODE_ENV = 'test';
+  });
+  it('handles errors with headers sent', () => {
+    res.headersSent = true;
+    errorHandler(err, req, res, next);
+
+    assert(next.called);
+  });
+  it('handles errors when in development mode', () => {
+    process.env.NODE_ENV = 'development';
+    errorHandler(err, req, res, next);
+
+    sinon.assert.calledOnce(jsonFunc);
+  });
+});
